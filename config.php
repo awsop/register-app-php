89ff1b1614e52de5cfa2b90f2cf454954d8c93fb
vadlamuricloudops <88264138+vadlamuricloudops@users.noreply.github.com>
2021-09-15 22:17:02 +0530

    Update config.php

--- config.php
+++ config.php
@@ -1,10 +1,10 @@
 <?php
 /* Database credentials. Assuming you are running MySQL
 server with default setting (user 'root' with no password) */
-define('DB_SERVER', 'localhost');
-define('DB_USERNAME', 'sbi');
-define('DB_PASSWORD', 'Shiva@3578');
-define('DB_NAME', 'sbiapp');
+define('DB_SERVER', 'phpapp.c1j72mmvruih.us-east-1.rds.amazonaws.com');
+define('DB_USERNAME', 'phpapp');
+define('DB_PASSWORD', 'phpapp123');
+define('DB_NAME', 'axies');
  
 /* Attempt to connect to MySQL database */
 $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
